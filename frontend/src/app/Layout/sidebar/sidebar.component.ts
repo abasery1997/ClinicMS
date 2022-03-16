25ba9cc4394db695f80ec1d5c34e8724ab57c869
Abdelrahman <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2022-03-16 15:05:24 +0200

    adding sidebar toggler

--- frontend/src/app/Layout/sidebar/sidebar.component.ts
+++ frontend/src/app/Layout/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,28 +9,17 @@ export class SidebarComponent implements OnInit {
   activeElement: HTMLElement | undefined;
   constructor() {
   }
+  ngAfterViewInit(): void {
+
+  }
   isToggle: boolean = false;
   ngOnInit(): void {
     //active navbar buttons
     let path = location.pathname.slice(1);
     this.active(document.getElementById((path == '') ? 'Home' : path)!);
-
-    //toggler
-    // let toggler = document.getElementById("toggler")!;
-    // toggler.addEventListener("click", function () {
-    //   this.parentElement!.style.width = "80px";
-    //   this.classList.replace("ms-auto", "text-center");
-    //   links.forEach((link) => {
-    //     link.classList.add("text-center");
-    //     link.children[0].classList.add("fs-5");
-    //     link.children[1].classList.add("d-none");
-    //     document.getElementById("userInfo")!.classList.add("d-none");
-    //   });
-    // });
   }
   toggle(): void {
     this.isToggle = !this.isToggle;
-    console.log(this.isToggle);
   }
   active(element: HTMLElement) {
     if (this.activeElement?.id != element.id) {
