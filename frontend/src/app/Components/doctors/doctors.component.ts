d6141de36e2ffe74189009aa52242696d45fb5d3
YOoOUNiieeSS <mohamedyounies96@gmail.com>
2022-03-24 00:33:10 +0200

    doctor is done witout validation

--- frontend/src/app/Components/doctors/doctors.component.ts
+++ frontend/src/app/Components/doctors/doctors.component.ts
@@ -34,7 +34,8 @@ export class DoctorsComponent implements OnInit, AfterViewInit {
   attendingDaysValues: string[] = ["sat", "sun", "mon", "tue", "wed", "thu", "fri"];
   parseWorkingDays(days: string) {
     this.attendingDaysString = days.split(',');
-    this.attendingDaysString.splice(this.attendingDaysString.length - 1, 1);
+    if(this.attendingDaysString.length>1)
+      this.attendingDaysString.splice(this.attendingDaysString.length - 1, 1);
     for (let i = 0; i < this.attendingDaysString.length; i++) {
       if (this.attendingDaysString[i] == "mon")
         this.attendingDaysString[i] = "Monday";
